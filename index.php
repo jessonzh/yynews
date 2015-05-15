656dc7576a499ae6f6a3819aef6be71861052525
jessonzh <zhjesson@gmail.com>
2015-05-15 23:26:59 +0800

    add all files

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>云影新闻网 | 首页</title>
+    <link rel="stylesheet" type="text/css" href="./css/reset.css">
+    <link rel="stylesheet" type="text/css" href="./css/header.css">
+    <link rel="stylesheet" type="text/css" href="./css/footer.css">
+</head>
+<body>
+<div id="container">
+    <?php
+    require ('./inc/header.inc');
+     ?>
+
+
+
+    <?php
+    require './inc/footer.inc';
+     ?>
+</div>
+</body>
+</html>
