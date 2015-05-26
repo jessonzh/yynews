e0845aea111012a47b8c3beda12520b98ecbc3c8
jessonzh <zhjesson@gmail.com>
2015-05-26 22:55:01 +0800

    change create sql

--- sql/create.sql
+++
@@ -1,6 +0,0 @@
-set names utf8;
-create database yynews;
-
-create table categories {
-    catID int primary key;
-}
---
+++ sql/create_database.sql
@@ -0,0 +1,24 @@
+set names utf8;
+create database yynews;
+use yynews;
+
+create table categories (
+    catId int AUTO_INCREMENT primary key,
+    catName char(30) not null unique
+)charset utf8;
+
+create table news (
+    newId int AUTO_INCREMENT primary key,
+    title varchar(100) not null unique,
+    content text not null,
+    createTime int not null,
+    catId int
+)charset utf8;
+
+create table comments (
+    commId int AUTO_INCREMENT primary key,
+    content text not null,
+    createTime int not null,
+    userIP char(15) not null,
+    newsId int
+)charset utf8;
