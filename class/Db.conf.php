1bc21a49123c335a4fdd0014d49140db0dcb2cc2
jessonzh <zhjesson@gmail.com>
2015-05-27 00:24:02 +0800

    add sql

---
+++ class/Db.conf.php
@@ -0,0 +1,9 @@
+<?php
+//功能：数据库的配置信息
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'yynews');
+define('DB_CHARSET', 'utf8');
+define('TIMEZONE', 'PRC');
+ ?>
---
+++ class/DbConnect.class.php
@@ -0,0 +1,35 @@
+<?php
+//功能：连接到数据库
+require_once('./Db.conf.php');
+date_default_timezone_set(TIMEZONE);
+class DbConnect
+{
+    private $host;
+    private $username;
+    private $password;
+    private $dbname;
+    public $conn;
+    public function _construct($host = DB_HOST, $username = DB_USER, $password = DB_PASSWORD, $dbname = DB_NAME)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->dbname = $dbname;
+    }
+    public function db_connect()
+    {
+        $this->conn = mysql_connect($this->host, $this->username, $this->password);
+        mysql_selectdb($this->dbname);
+        mysql_query("set names utf8", $this->conn);
+    }
+}
+
+/*
+//测试代码
+$db = new DbConnect();
+$db->db_connect();
+var_dump($db);
+*/
+
+
+ ?>
